cd8071e75181c27fcb58899115a1de37f28bd69f
dragonmastermm <mwmills03@outlook.com>
2025-10-04 14:29:29 -0400

    no

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepadpractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepadpractice.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 public class gamepadpractice extends OpMode {
 
