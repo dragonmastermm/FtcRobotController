70be7cff548004a6457a2eaf7f672194c1be8fe9
dragonmastermm <mwmills03@outlook.com>
2025-10-16 19:34:39 -0400

    stuff

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -401,8 +401,6 @@ public class FtcRobotControllerActivity extends Activity
       initWifiMute(true);
     }
 
-    FtcAboutActivity.setBuildTimeFromBuildConfig(BuildConfig.APP_BUILD_TIME);
-
     // check to see if there is a preferred Wi-Fi to use.
     checkPreferredChannel();
 
