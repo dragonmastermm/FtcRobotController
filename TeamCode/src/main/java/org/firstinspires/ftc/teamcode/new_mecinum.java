a168c33a55a29f286777f106df461f0cfa14ac83
dragonmastermm <mwmills03@outlook.com>
2025-10-18 11:29:42 -0400

    fix code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/new_mecinum.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class new_mecinum extends OpMode {
+
+    private DcMotor intake;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void init() {
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+    }
+
+    @Override
+    public void loop() {
+
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
+
+        double frontLeftPower = drive + strafe + rotate;
+        double frontRightPower = drive - strafe - rotate;
+        double backLeftPower = drive - strafe + rotate;
+        double backRightPower = drive + strafe - rotate;
+
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+
+        if (gamepad1.a) {
+            intake.setPower(1.0); // Intake in
+        } else if (gamepad1.b) {
+            intake.setPower(-1.0); // Intake out
+        } else {
+            intake.setPower(0); // Stop intake
+        }
+
+    }
+
+}
